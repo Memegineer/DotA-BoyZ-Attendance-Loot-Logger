4e84ad15e4632c21910d3a9cbcd9dd2bf763c09f
Jake <jacob37857@gmail.com>
2020-05-27 19:00:39 -0400

    Small stupid test

--- DotA BoyZ Attendance Loot Logger/DotABoyZ.Master.designer.cs
+++ DotA BoyZ Attendance Loot Logger/DotABoyZ.Master.designer.cs
@@ -19,7 +19,7 @@ namespace DotA_BoyZ_Attendance_Loot_Logger {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head ;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
